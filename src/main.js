311a18ea1f3cc81ffe03f85fc5473911d44dccf5
Maik Fischer <m.fischer@beyond-interfaces.com>
2020-12-21 14:33:30 +0100

    Add AOS dependency

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import '~/assets/styles.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import DefaultLayout from '~/layouts/Default.vue';
 
 export default function(Vue, { router, head, isClient }) {
+  const config = {
+    duration: 400,
+    once: true
+  };
+  AOS.init(config);
+  Vue.mixin({
+    updated() {
+      this.$nextTick(function() {
+        AOS.refreshHard(); // This is needed to avoid the un-animate aos effect
+      });
+    }
+  });
+
   Vue.component('Layout', DefaultLayout);
 
   head.meta.push({
